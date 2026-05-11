710b31b2c1fd050c90f439ec00317942ac3bcf76
Oxcode <mrexcelsam1@gmail.com>
2026-05-11 12:23:41 +0100

    modify

--- src/validations/profileSchema.ts
+++ src/validations/profileSchema.ts
@@ -9,7 +9,12 @@ export const userDetailsSchema = z.object({
 })
 
 export const changePasswordSchema = z.object({
-    password: z.string().min(6, "Password must be at least 6 characters"), 
+    password: z.string().min(6, "Password must be at least 6 characters")
+        .regex(
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/,
+            "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
+        )
+,
     confirm_password: z.string().min(6, "Confirm password must be at least 6 characters")
 }).refine((data) => data.password === data.confirm_password, {
     message: "Passwords don't match", path: ["confirm_password"]
