5951e73154b59738e25a8cfb0c511c2846d6d4ae
Oxcode <mrexcelsam1@gmail.com>
2026-05-13 11:12:04 +0100

    modify

--- src/services/PostService.ts
+++ src/services/PostService.ts
@@ -1,5 +1,5 @@
 import { Post } from "../models/post.ts"
-import { IPost } from "../types/index.ts"
+import { type IPost } from "../types/index.ts"
 
 export const storePost = async (data: Pick<IPost, 'caption' | 'image' | 'postedBy'>) => {
     return await Post.create(data)
