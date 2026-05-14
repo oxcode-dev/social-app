6e3e64fcdb22d90f41a31f071b4ba8172cc0a2ad
Oxcode <mrexcelsam1@gmail.com>
2026-05-14 11:22:35 +0100

    modify

--- src/services/PostService.ts
+++ src/services/PostService.ts
@@ -58,3 +58,30 @@ export const storePostComment = async (postId: string, userId: string, comment: 
 
     return false;
 }
+
+
+export const likeUnlikePostSystem = async (postId: string, userId: string) => {
+    const post = await fetchPost(postId)
+
+    if (!post) {
+        return false;
+        // return res.status(404).send({
+        //     message: "Post not found!",
+        // })
+    }
+
+    let responseMessage;
+
+    if (post.likes.includes(userId)) {
+        const index = post.likes.indexOf(userId);
+        post.likes.splice(index, 1);
+        await post.save();
+        responseMessage = "Post Unliked successfully"
+    } else {
+        post.likes.push(userId)
+        await post.save();
+        responseMessage = "Post Liked successfully"
+    }
+
+    return responseMessage;
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -52,7 +52,7 @@ export interface IPost {
     caption: string;
     image?: string | null;
     postedBy: string | mongoose.Schema.Types.ObjectId;
-    likes: mongoose.Schema.Types.ObjectId[];
+    likes: string[];// | mongoose.Schema.Types.ObjectId[];
     comments: {
         user: string | mongoose.Schema.Types.ObjectId;
         comment: string;
