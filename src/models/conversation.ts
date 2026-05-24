ec7277ab442b3605268e6b01eed7e1aa0f24f496
Oxcode <mrexcelsam1@gmail.com>
2026-05-24 22:35:18 +0100

    modify

--- src/models/conversation.ts
+++ src/models/conversation.ts
@@ -32,3 +32,4 @@ conversationSchema.set('toObject', { virtuals: true });
 conversationSchema.set('toJSON', { virtuals: true });
 
 export const Conversation = mongoose.model('social_conversations', conversationSchema);
+
