77f37a57f926d626918ac3c87c02eba56fc1e83c
Oxcode <mrexcelsam1@gmail.com>
2026-06-19 23:47:41 +0100

    modify

--- src/middlewares/handleErrors.ts
+++ src/middlewares/handleErrors.ts
@@ -1,20 +1,21 @@
 import express from "express";
+import { isProduction } from "../config/index.ts";
 
 
 export const handleUnknownRoute = (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    res.status(404).json({
-        success: false,
-        message: `Can't find ${req.originalUrl} on this server!`
-    });
+  res.status(404).json({
+    success: false,
+    message: `Can't find ${req.originalUrl} on this server!`
+  });
 };
 
 export const handleError = async (err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
-    console.error(err.stack);
-    res.status(err.status || 500).json({
-        error: process.env.NODE_ENV === 'production' 
-            ? 'Internal Server Error' 
-            : err.message
-    });
+  console.error(err.stack);
+  res.status(err.status || 500).json({
+    error: isProduction
+      ? 'Internal Server Error' 
+      : err.message
+  });
 };
 
 const errorHandler = (err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
