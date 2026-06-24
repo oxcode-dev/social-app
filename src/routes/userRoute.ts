68d895d0f5515932ac287f68f9d24bb5adfffbb3
Oxcode <mrexcelsam1@gmail.com>
2026-06-25 00:40:17 +0100

    modify

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { followUser, getAllUsers, getSuggestedUsers, getUserDetails, getUserFollowers, getUserFollowings, unfollowUser } from '../controllers/userController.ts';
+import { getAllUsers, getSuggestedUsers } from '../controllers/userController.ts';
 import { auth } from '../middlewares/authMiddleware.ts';
 import { handlePagination } from '../middlewares/handlePagination.ts';
 
@@ -9,15 +9,5 @@ router.route('/')
     .get(getAllUsers)
 
 router.get("/suggestions", auth, handlePagination as any, getSuggestedUsers);
-
-router.put('/:id/follow', auth, followUser as any)
-
-router.put('/:id/unfollow', auth, unfollowUser as any)
-
-router.get('/:id/followers', auth, getUserFollowers)
-
-router.get('/:id/followings', auth, getUserFollowings)
-
-router.get('/:id', getUserDetails)
 
 export { router as userRouter };
