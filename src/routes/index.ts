e79b6e2709e17dd1d1347db2eca7824c7cbaa614
Oxcode <mrexcelsam1@gmail.com>
2026-06-27 02:09:17 +0100

    modify

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,6 +7,7 @@ import { postsRouter } from './postRouter.ts';
 import { chatsRouter } from './chatRoute.ts';
 import { userRouter } from './userRoute.ts';
 import { followingsRouter } from './followingsRoute.ts';
+import { Post } from '../models/post.ts';
 
 const routes = (app: express.Application) => {
     app.use('/api/auth', authRouter)
@@ -17,8 +18,13 @@ const routes = (app: express.Application) => {
     app.use('/api/users', userRouter)
     app.use('/api/followings', followingsRouter)
 
-    app.get('/api/test', (req, res) => {
+    app.get('/api/test', async (req, res) => {
         res.status(200).json({ message: 'Testing API routes' })
+
+        // const posts = await Post.find({
+        //     'postedBy': '699f80cf00d4b770db122aa5'
+        // })
+        // res.status(200).json({ 'posts': posts })
     })
 }
 
