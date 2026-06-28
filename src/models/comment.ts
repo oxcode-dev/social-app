910bb6b2edcc4916f06eaf8ce3e72ff2454f1ae4
Oxcode <mrexcelsam1@gmail.com>
2026-06-28 01:32:34 +0100

    modify

---
+++ src/models/comment.ts
@@ -0,0 +1,53 @@
+// models/Comment.js
+
+import mongoose, { Schema } from "mongoose";
+
+
+const commentSchema = new mongoose.Schema({
+
+    post:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Post",
+        required:true
+    },
+
+
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+
+
+    text:{
+        type:String,
+        required:true
+    },
+
+
+    parentComment:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Comment",
+        default:null
+    },
+
+
+    likes:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User"
+        }
+    ],
+
+
+    repliesCount:{
+        type:Number,
+        default:0
+    }
+
+
+},{
+    timestamps:true
+});
+
+export const Post = mongoose.model('social_post_comments', commentSchema);
