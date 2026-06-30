825437bf5f80abe298deed0903df29ab3ccd23f6
Oxcode <mrexcelsam1@gmail.com>
2026-06-30 13:25:15 +0100

    modify

--- src/controllers/CommentController.ts
+++ src/controllers/CommentController.ts
@@ -28,6 +28,37 @@ export const addComments = async (req: express.Request | any, res: express.Respo
 
     return res.status(200).json({
         success: true,
+        message: "Comment Added",
+        comment: response,
+    });
+}
+
+export const replyToComment = async (req: express.Request | any, res: express.Response) => {
+    const postId = req.params.id as string;
+
+    const auth = req.user;
+
+    const text = req.body.text as string;
+
+    const post = await fetchPost(postId);
+
+    if(!post) {
+        return res.status(404).json({
+            status: "error",
+            message: "Post not found!",
+        })
+    }
+
+    
+    let response = await storePostComment({
+        postId, 
+        userId: auth.id, 
+        text,
+        parentCommentId: null
+    });
+
+    return res.status(200).json({
+        success: true,
         message: "Comment Added"
     });
 }
