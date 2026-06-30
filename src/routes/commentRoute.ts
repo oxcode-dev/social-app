a524c2e8b1614bf0625d53471431f2600dc77a73
Oxcode <mrexcelsam1@gmail.com>
2026-07-01 00:39:19 +0100

    modify

--- src/routes/commentRoute.ts
+++ src/routes/commentRoute.ts
@@ -1,11 +1,18 @@
 import express from 'express';
 import { auth } from '../middlewares/authMiddleware.ts';
-import { getComment } from '../controllers/CommentController.ts';
+import { deleteComment, getComment, getCommentReplies, likePostComment, unLikePostComment } from '../controllers/CommentController.ts';
 
 const router = express.Router();
 
+router.put('/:id/like', auth, likePostComment);
+router.put('/:id/unlike', auth, unLikePostComment);
+
+router.route('/:id/replies')
+    .post(auth, getCommentReplies)
+    .get(auth, getCommentReplies);
+
 router.route('/:id')
-    .delete(auth, getComment)
+    .delete(auth, deleteComment)
     .get(auth, getComment);
 
 export { router as commentRouter };
