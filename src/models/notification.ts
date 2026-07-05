0977167f7c0ff18cd2668c56dc889d17d17adff3
Oxcode <mrexcelsam1@gmail.com>
2026-07-05 16:55:19 +0100

    initial commit

---
+++ src/models/notification.ts
@@ -0,0 +1,58 @@
+import mongoose from "mongoose";
+
+const notificationSchema = new mongoose.Schema({
+    recipient: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+
+    sender: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+
+    type: {
+        type: String,
+        enum: [
+            "FOLLOW",
+            "LIKE",
+            "COMMENT",
+            "COMMENT_REPLY",
+            "MESSAGE",
+            "MENTION"
+        ],
+        required: true
+    },
+
+    post: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post"
+    },
+
+    comment: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment"
+    },
+
+    message: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Message"
+    },
+
+    isRead: {
+        type: Boolean,
+        default: false
+    }
+
+},{
+    timestamps:true
+});
+
+notificationSchema.index({
+    recipient:1,
+    createdAt:-1
+});
+
+export const Notification = mongoose.model('social_notifications', notificationSchema);
