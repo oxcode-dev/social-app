8a5c6694dce5b046a346f23343ff8ec5ce22cb87
Oxcode <mrexcelsam1@gmail.com>
2026-07-05 17:05:49 +0100

    initial commit

---
+++ src/services/notificationService.ts
@@ -0,0 +1,49 @@
+import { Notification } from "../models/notification.ts";
+import { NotificationType } from "../types/index.ts";
+
+
+export const createNotification = async({
+
+    io,
+
+    recipient,
+
+    sender,
+
+    type,
+
+    post=null,
+
+    comment=null,
+
+    message=null
+
+}: NotificationType) =>{
+
+    if(recipient.toString()===sender.toString()){
+        return;
+    }
+
+    const notification = await Notification.create({
+
+        recipient,
+
+        sender,
+
+        type,
+
+        post,
+
+        comment,
+
+        message
+
+    });
+
+    const populated = await Notification.findById(notification._id)
+        .populate("sender","username profilePic");
+
+    io.to(recipient.toString())
+        .emit("notification", populated);
+
+};
