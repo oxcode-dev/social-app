8d485686305f0cef10ca6aa0454cfed7921970d3
Oxcode <mrexcelsam1@gmail.com>
2026-07-08 00:05:11 +0100

    initial commit

--- src/routes/notificationRoute.ts
+++ src/routes/notificationRoute.ts
@@ -2,3 +2,13 @@
 // PATCH  /api/notifications/:notificationId/read
 // PATCH  /api/notifications/read-all
 // DELETE /api/notifications/:notificationId
+
+import express from 'express';
+import { auth } from '../middlewares/authMiddleware.ts';
+import { getAllUserNotifications } from '../controllers/NotificationController.ts';
+
+const router = express.Router();
+
+router.get('/', auth, getAllUserNotifications as any);
+
+export { router as notificationRouter  };
