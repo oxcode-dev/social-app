2ddee74a2d1d40deaa5210e302361f1827aa5b7b
Oxcode <mrexcelsam1@gmail.com>
2026-07-08 00:08:08 +0100

    initial commit

--- src/routes/notificationRoute.ts
+++ src/routes/notificationRoute.ts
@@ -1,14 +1,13 @@
-// GET    /api/notifications
-// PATCH  /api/notifications/:notificationId/read
-// PATCH  /api/notifications/read-all
-// DELETE /api/notifications/:notificationId
-
 import express from 'express';
 import { auth } from '../middlewares/authMiddleware.ts';
-import { getAllUserNotifications } from '../controllers/NotificationController.ts';
+import { deleteNotification, getAllUserNotifications, markAllNotificationsRead, markNotificationRead } from '../controllers/NotificationController.ts';
 
 const router = express.Router();
 
 router.get('/', auth, getAllUserNotifications as any);
+router.patch('/:notificationId/read', auth, markNotificationRead as any);
+router.patch('/read-all', auth, markAllNotificationsRead as any);
+
+router.delete('/:notificationId', auth, deleteNotification as any)
 
 export { router as notificationRouter  };
