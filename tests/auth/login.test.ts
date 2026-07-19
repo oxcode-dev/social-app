a8253ba9ab22d29dec014b0075033bd0752f5fee
Oxcode <mrexcelsam1@gmail.com>
2026-07-19 01:45:26 +0100

    initial commit

---
+++ tests/auth/login.test.ts
@@ -0,0 +1,93 @@
+// tests/auth/register.test.ts
+
+import request from "supertest";
+import bcrypt from "bcryptjs";
+import { describe, it, expect } from "vitest";
+import app from "../../src/index.ts";
+import { User } from "../../src/models/user.ts";
+
+
+describe("Login", () => {
+
+    const endpoint = "/api/auth/login";
+
+    const payload = {
+        email: "sam@example.com",
+        password: "Password123!"
+    };
+
+    it("should login an existing user", async () => {
+
+        const response = await request(app)
+            .post(endpoint)
+            .send(payload);
+
+        expect(response.status).toBe(201);
+
+        expect(response.body.success).toBe(true);
+
+        expect(response.body.user.email)
+            .toBe(payload.email);
+
+    });
+
+    it("should hash password", async () => {
+
+        await request(app)
+            .post(endpoint)
+            .send(payload);
+
+        const user = await User.findOne({
+            email: payload.email
+        }).select("+password");
+
+        expect(user).not.toBeNull();
+
+        const match = await bcrypt.compare(
+            payload.password,
+            user!.password
+        );
+
+        expect(match).toBe(true);
+
+    });
+
+    it("should reject duplicate email", async () => {
+
+        await User.create(payload);
+
+        const response = await request(app)
+            .post(endpoint)
+            .send(payload);
+
+        expect(response.status).toBe(400);
+
+    });
+
+    it("should reject invalid email", async () => {
+
+        const response = await request(app)
+            .post(endpoint)
+            .send({
+                ...payload,
+                email: "invalid"
+            });
+
+        expect(response.status).toBe(400);
+
+    });
+
+    it("should reject weak password", async () => {
+
+        const response = await request(app)
+            .post(endpoint)
+            .send({
+                ...payload,
+                password: "123"
+            });
+
+        expect(response.status).toBe(400);
+
+    });
+
+});
