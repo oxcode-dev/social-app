9b8c29270ed88ca62646ab6c5cbdae984fd53a26
Oxcode <mrexcelsam1@gmail.com>
2026-07-31 00:22:02 +0100

    initial commit

--- src/config/socket.ts
+++ src/config/socket.ts
@@ -1,105 +1,36 @@
-import { Server as HttpServer } from 'http';
 import { Socket, Server } from 'socket.io';
-import { v4 } from 'uuid';
 
-export class ServerSocket {
-    public static instance: ServerSocket;
-    public io: Server;
+const onlineUsers = new Map();
 
-    /** Master list of all connected users */
-    public users: { [uid: string]: string };
+const SocketServer = (socket: Socket) => {
 
-    constructor(server: HttpServer) {
-        ServerSocket.instance = this;
-        this.users = {};
-        this.io = new Server(server, {
-            serveClient: false,
-            pingInterval: 10000,
-            pingTimeout: 5000,
-            cookie: false,
-            cors: {
-                origin: '*'
-            }
-        });
+    socket.on("join", (userId) => {
+        onlineUsers.set(userId, socket.id);
+    });
 
-        this.io.on('connect', this.StartListeners);
+    socket.on("send-message", async (data) => {
 
-        console.info('Socket server started');
-    }
+        const receiverSocket = onlineUsers.get(data.receiverId);
 
-    StartListeners = (socket: Socket) => {
-        console.info('Message received from ' + socket.id);
-
-        socket.on('handshake', (callback: (uid: string, users: string[]) => void) => {
-            console.info('Handshake received from: ' + socket.id);
-
-            const reconnected = Object.values(this.users).includes(socket.id);
-
-            if (reconnected) {
-                console.info('This user has reconnected.');
-
-                const uid = this.GetUidFromSocketID(socket.id);
-                const users = Object.values(this.users);
-
-                if (uid) {
-                    console.info('Sending callback for reconnect ...');
-                    callback(uid, users);
-                    return;
-                }
-            }
-
-            const uid = v4();
-            this.users[uid] = socket.id;
-
-            const users = Object.values(this.users);
-            console.info('Sending callback ...');
-            callback(uid, users);
-
-            this.SendMessage(
-                'user_connected',
-                users.filter((id) => id !== socket.id),
-                users
+        if (receiverSocket) {
+            socket.to(receiverSocket).emit(
+                "receive-message",
+                data
             );
-        });
+        }
 
-        socket.on('disconnect', () => {
-            console.info('Disconnect received from: ' + socket.id);
+    });
 
-            const uid = this.GetUidFromSocketID(socket.id);
+    socket.on("disconnect", () => {
 
-            if (uid) {
-                delete this.users[uid];
+        for (const [userId, socketId] of onlineUsers) {
 
-                const users = Object.values(this.users);
-
-                this.SendMessage('user_disconnected', users, socket.id);
+            if (socket.id === socketId) {
+                onlineUsers.delete(userId);
             }
-        });
 
-        socket.on("join", (userId) => {
-            socket.join(userId);
-            // onlineUsers.set(userId, socket.id);
-        });
+        }
 
-        socket.on("send-message", ({ receiverId, message }) => {
-            this.io.to(receiverId).emit("new-message", message);
-        });
+    });
 
-        socket.on("typing", ({ receiverId }) => {
-            this.io.to(receiverId).emit("typing");
-        });
-
-        socket.on("stop-typing", ({ receiverId }) => {
-            this.io.to(receiverId).emit("stop-typing");
-        });
-    };
-
-    GetUidFromSocketID = (id: string) => {
-        return Object.keys(this.users).find((uid) => this.users[uid] === id);
-    };
-
-    SendMessage = (name: string, users: string[], payload?: Object) => {
-        console.info('Emitting event: ' + name + ' to', users);
-        users.forEach((id) => (payload ? this.io.to(id).emit(name, payload) : this.io.to(id).emit(name)));
-    };
-}
+};
